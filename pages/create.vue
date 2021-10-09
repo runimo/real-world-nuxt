dcc2f5a50bf59373ec85f2f883833b62d3b2283a
runimo <63205791+runimo@users.noreply.github.com>
2021-10-09 17:51:01 +0200

    feat: use vue-meta for SEO

--- pages/create.vue
+++ pages/create.vue
@@ -5,5 +5,18 @@
 </template>
 
 <script>
-export default {}
+  export default {
+    head() {
+      return {
+        title: 'Create an Event',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'You can create a new event in your neighborhood'
+          }
+        ]
+      }
+    }
+  }
 </script>
