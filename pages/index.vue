19e3acc8c58717f4ad87a6fa45a6365d530e4bd6
runimo <63205791+runimo@users.noreply.github.com>
2021-10-10 08:54:40 +0200

    feat: fetch events via API

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
     <h1>Events</h1>
+
+    <EventCard
+      v-for="(event, idx) in events"
+      :key="idx"
+      :event="event"
+      :data-index="idx" />
   </div>
 </template>
 
 <script>
-  export default {
-    head() {
-      return {
-        title: 'Event Listing'
-      }
+import EventCard from '~/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  },
+
+  head() {
+    return {
+      title: 'Event Listing'
     }
+  },
+
+  asyncData({ $axios, error }) {
+    return $axios.get('http://localhost:3000/events')
+      .then(response => {
+        return {
+          events: response.data
+        }
+      })
+      .catch(e => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again.' })
+      })
   }
+}
 </script>
